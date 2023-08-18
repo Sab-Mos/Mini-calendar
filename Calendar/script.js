e9faf1a903f1df6670b98fdad20ab42fe9b418ac
Sab-Mos <131381168+Sab-Mos@users.noreply.github.com>
2023-08-18 11:22:25 +0400

    Add files via upload

---
+++ Calendar/script.js
@@ -0,0 +1,15 @@
+const date = document.querySelector('.date');
+const day = document.querySelector('.day');
+const month = document.querySelector('.month');
+const year = document.querySelector('.year');
+
+const today = new Date();
+
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+date.textContent = (today.getDate()< 10 ? '0' : '') + today.getDate();
+day.textContent = weekDays[today.getDay()];
+month.textContent = allMonths[today.getMonth()];
+year.textContent = today.getFullYear();
